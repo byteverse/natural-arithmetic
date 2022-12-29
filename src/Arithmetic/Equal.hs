618cbcc3cb9002e67373db9fecec97c0951fd17f
Andrew Martin <andrew.thaddeus@gmail.com>
2022-12-29 11:07:13 -0500

    Add more inline pragmas to prevent tiny cost centers

--- src/Arithmetic/Equal.hs
+++ src/Arithmetic/Equal.hs
@@ -13,10 +13,13 @@ import Arithmetic.Unsafe (type (:=:)(Eq))
 import GHC.TypeNats (type (+))
 
 symmetric :: (m :=: n) -> (n :=: m)
+{-# inline symmetric #-}
 symmetric Eq = Eq
 
 plusL :: forall c m n. (m :=: n) -> (c + m :=: c + n)
+{-# inline plusL #-}
 plusL Eq = Eq
 
 plusR :: forall c m n. (m :=: n) -> (m + c :=: n + c)
+{-# inline plusR #-}
 plusR Eq = Eq
--- src/Arithmetic/Lt.hs
+++ src/Arithmetic/Lt.hs
@@ -37,72 +37,87 @@ import GHC.TypeNats (CmpNat,type (+))
 import qualified GHC.TypeNats as GHC
 
 toLteR :: (a < b) -> (a + 1 <= b)
+{-# inline toLteR #-}
 toLteR Lt = Lte
 
 toLteL :: (a < b) -> (1 + a <= b)
+{-# inline toLteL #-}
 toLteL Lt = Lte
 
 -- | Replace the left-hand side of a strict inequality
 -- with an equal number.
 substituteL :: (b :=: c) -> (b < a) -> (c < a)
+{-# inline substituteL #-}
 substituteL Eq Lt = Lt
 
 -- | Replace the right-hand side of a strict inequality
 -- with an equal number.
 substituteR :: (b :=: c) -> (a < b) -> (a < c)
+{-# inline substituteR #-}
 substituteR Eq Lt = Lt
 
 -- | Add a strict inequality to a nonstrict inequality.
 plus :: (a < b) -> (c <= d) -> (a + c < b + d)
+{-# inline plus #-}
 plus Lt Lte = Lt
 
 -- | Add a constant to the left-hand side of both sides of
 -- the strict inequality.
 incrementL :: forall (c :: GHC.Nat) (a :: GHC.Nat) (b :: GHC.Nat).
   (a < b) -> (c + a < c + b)
+{-# inline incrementL #-}
 incrementL Lt = Lt
 
 -- | Add a constant to the right-hand side of both sides of
 -- the strict inequality.
 incrementR :: forall (c :: GHC.Nat) (a :: GHC.Nat) (b :: GHC.Nat).
   (a < b) -> (a + c < b + c)
+{-# inline incrementR #-}
 incrementR Lt = Lt
 
 -- | Add a constant to the left-hand side of the right-hand side of
 -- the strict inequality.
 weakenL :: forall (c :: GHC.Nat) (a :: GHC.Nat) (b :: GHC.Nat).
   (a < b) -> (a < c + b)
+{-# inline weakenL #-}
 weakenL Lt = Lt
 
 -- | Add a constant to the right-hand side of the right-hand side of
 -- the strict inequality.
 weakenR :: forall (c :: GHC.Nat) (a :: GHC.Nat) (b :: GHC.Nat).
   (a < b) -> (a < b + c)
+{-# inline weakenR #-}
 weakenR Lt = Lt
 
 -- | Compose two strict inequalities using transitivity.
 transitive :: (a < b) -> (b < c) -> (a < c)
+{-# inline transitive #-}
 transitive Lt Lt = Lt
 
 -- | Compose a strict inequality (the first argument) with a nonstrict
 -- inequality (the second argument).
 transitiveNonstrictR :: (a < b) -> (b <= c) -> (a < c)
+{-# inline transitiveNonstrictR #-}
 transitiveNonstrictR Lt Lte = Lt
 
 transitiveNonstrictL :: (a <= b) -> (b < c) -> (a < c)
+{-# inline transitiveNonstrictL #-}
 transitiveNonstrictL Lte Lt = Lt
 
 -- | Zero is less than one.
 zero :: 0 < 1
+{-# inline zero #-}
 zero = Lt
 
 -- | Nothing is less than zero.
 absurd :: n < 0 -> void
-absurd Lt = error "Arithmetic.Nat.absurd: n < 0"
+{-# inline absurd #-}
+absurd Lt = errorWithoutStackTrace "Arithmetic.Nat.absurd: n < 0"
 
 -- | Use GHC's built-in type-level arithmetic to prove
 -- that one number is less than another. The type-checker
 -- only reduces 'CmpNat' if both arguments are constants.
 constant :: forall a b. (CmpNat a b ~ 'LT) => (a < b)
+{-# inline constant #-}
 constant = Lt
 

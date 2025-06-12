05a7efaa91b88a5494fe6c8e586c88b46db6ebff
Andrew Martin <andrew.thaddeus@gmail.com>
2025-06-12 12:29:23 -0400

    Add unlifted variant of commutative proof for addition

--- src/Arithmetic/Plus.hs
+++ src/Arithmetic/Plus.hs
@@ -2,16 +2,20 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ExplicitForAll #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE MagicHash #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UnboxedTuples #-}
 
 module Arithmetic.Plus
   ( zeroL
   , zeroR
   , commutative
+  , commutative#
   , associative
   ) where
 
 import Arithmetic.Unsafe (type (:=:) (Eq))
+import Arithmetic.Unsafe (type (:=:#) (Eq#))
 import GHC.TypeNats (type (+))
 
 -- | Any number plus zero is equal to the original number.
@@ -25,6 +29,9 @@ zeroL = Eq
 -- | Addition is commutative.
 commutative :: forall a b. a + b :=: b + a
 commutative = Eq
+
+commutative# :: forall a b. (# #) -> a + b :=:# b + a
+commutative# _ = Eq# (# #)
 
 -- | Addition is associative.
 associative :: forall a b c. (a + b) + c :=: a + (b + c)
